a20ea4d509a9f7384c67f50d3e71e589566dfbab
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-05 15:38:27 -0500

    feat: Add solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+function isNegative(num) {
+    return num < 0;
+}
 
+function isNotInteger(num) {
+    return (num % 1) !== 0;
+}
+
+function isNotNumberType(num) {
+    return (typeof num) !== "number";
+}
+
+const sumAll = function (first, second) {
+    if (
+        isNegative(first) || isNegative(second) ||
+        isNotInteger(first) || isNotInteger(second) ||
+        isNotNumberType(first) || isNotNumberType(second)
+    ) return "ERROR";
+
+    if (first > second) {
+        // Swaps values with array destructuring
+        [first, second] = [second, first]
+    }
+
+    let sum = 0;
+    for (let i = first; i <= second; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

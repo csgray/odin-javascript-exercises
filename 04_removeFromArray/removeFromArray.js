e9ad9dc2cfcfd1b755f35d8d61c2f0fb52059aeb
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-05 14:57:52 -0500

    feat: Add solution to 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (items, ...args) {
+    for (const arg of args) {
+        // Returns an array of items that do not match the argument
+        items = items.filter((item) => item !== arg)
+    }
+
+    return items;
 };
 
 // Do not edit below this line

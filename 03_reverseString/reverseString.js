b95e77d7c0a28f9f28d6ec8d9fa6a598e1b2744a
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-04 19:56:41 -0500

    feat: Add solution to 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (str) {
+    const characters = str.split('');
+    const length = str.length;
+    let output = "";
 
+    for (i = 0; i < length; i++) {
+        output += characters.pop();
+    }
+
+    return output;
 };
 
 // Do not edit below this line

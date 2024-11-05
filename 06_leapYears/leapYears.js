5710fdf96fced45895866e09650ef9ab9c6b6dba
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-05 15:50:52 -0500

    feat: Add solution to 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    return (
+        // Leap years are divisible by four
+        year % 4 === 0 &&
+        // but not 100 unless they are divisible by 400
+        (year % 400 === 0 || year % 100 !== 0)
+    )
 };
 
 // Do not edit below this line

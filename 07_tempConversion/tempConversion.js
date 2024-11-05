d30d27439663ed9a0c92638d7c6d96b1ace990cc
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-05 16:08:30 -0500

    feat: Add solution to 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  // toFixed rounds to 1 decimal place but returns a string
+  return Number(((fahrenheit - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  // toFixed rounds to 1 decimal place but returns a string
+  return Number((celsius * 9 / 5 + 32).toFixed(1));
 };
 
 // Do not edit below this line

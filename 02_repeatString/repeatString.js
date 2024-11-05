f13589160bad1d6ec47249f92ef0558a351d14e3
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-04 19:51:10 -0500

    feat: Add solution to 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (str, count) {
+    if (count < 0) { return "ERROR" };
 
+    let output = "";
+
+    for (i = 0; i < count; i++) {
+        output += str;
+    }
+
+    return output;
 };
 
 // Do not edit below this line

de877e35868aa914c75dc28aaef908a5d2b20b9c
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-08 22:07:52 -0500

    feat: Add solution to 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function (people) {
+    const oldest = people.map(function (person) {
+        currentYear = new Date().getFullYear();
+        const age = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        return {
+            name: person.name,
+            age: age,
+        }
+    }).sort((a, b) => b.age - a.age)[0];
+    return people.find((person) => person.name === oldest.name)
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;

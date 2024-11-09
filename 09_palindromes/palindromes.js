6770b4fbf475f26a515e28dbe12329fa68fbf22d
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-08 20:41:26 -0500

    feat: Add solution to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    chars = str
+        .toLowerCase()
+        .split('')
+        .filter((char) => /[A-Za-z0-9]/.test(char));
+    return chars.join() === chars.reverse().join();
 };
 
 // Do not edit below this line

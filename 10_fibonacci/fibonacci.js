9090eccaeaf7d8f1e36f951f8735f59d0ec6b6ab
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-08 21:37:07 -0500

    feat: Add solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    if (index < 0) return "OOPS";
 
+    const numbers = [0, 1];
+    for (i = 2; i <= index; i++) {
+        numbers[i] = numbers[i - 2] + numbers[i - 1];
+    }
+    return numbers[index];
 };
 
 // Do not edit below this line
